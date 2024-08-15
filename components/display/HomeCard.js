453e114c28f93368d4c68c359eb3c30f1344335d
shenghanqin <shenghanqin@hujiang.com>
2024-08-15 15:14:34 +0800

    update home page

--- components/display/HomeCard.js
+++ components/display/HomeCard.js
@@ -5,15 +5,19 @@ import Link from "next/link";
 const HomeCards = (props) => {
   const { MovieCard } = props;
   const { show } = MovieCard
-  const { id, image, name } = show
+  const { id, image, name, rating } = show
 
   let poster_path = image
     ? image.medium
     : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
 
+  const ratingShow = rating.average
+    ? rating.average
+    : "No rating"
+
   return (
     <div className="w-64 h-96 sm:w-52 sm:h-72 bg-grey m-3 hover:opacity-75 shadow-md">
-      <Link key={id} href="/movie/[id]" as={`/movie/${id}`} title={name}>
+      <Link key={id} href="/movie/[id]" as={`/movie/${id}`} title={name} className="relative">
         <Image
           src={poster_path}
           alt={name}
@@ -22,6 +26,10 @@ const HomeCards = (props) => {
           height={288}
           unoptimized
         />
+        <div className="absolute bottom-0 left-0 right-0 bg-slate-400 p-2" style={{ background: 'rgba(0, 0, 0, 0.7)' }}>
+          <h3 className="text-white">{name}</h3>
+          <h4 className="text-primary">{ratingShow}</h4>
+        </div>
       </Link>
     </div>
   );
--- components/title/Title.js
+++ components/title/Title.js
@@ -4,8 +4,7 @@ const Title = () => {
   return (
     <div className="flex place-content-center mt-5 mx-5">
       <h1 className="w-full md:w-1/2 lg:w-1/3 text-white text-xl sm:text-2xl md:text-3xl text-center font-semibold">
-        <span className="text-primary">Xiaoxili</span>matic adventures await, start{" "}
-        <span className="text-primary">stream</span>ing now
+        <span className="text-primary">Xiaoxili</span> World
       </h1>
     </div>
   );
